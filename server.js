5be7a3ac7f499be80a69b0c3866625a07ef995cd
lyric <yiyuanwang33@gmail.com>
2019-12-03 20:48:48 +0800

    a

--- server.js
+++ server.js
@@ -26,12 +26,20 @@ var server = http.createServer(function(request, response) {
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`二哈`);
+    response.write(`
+      <DOCTYPE html>
+      <head>
+        <link rel="stylesheet" href="/x"
+      </head>
+      <body>
+        <h1>标题</h1>
+      </body>
+      `);
     response.end();
   } else if (path === "/x") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
-    response.write(`body{color: red;}`);
+    response.write(`h1{color: red;}`);
     response.end();
   } else {
     response.statusCode = 404;
