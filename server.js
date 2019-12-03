e26748698f5fa6570ab0603b1d69e6229c547c4d
lyric <yiyuanwang33@gmail.com>
2019-12-03 20:56:14 +0800

    a

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ var server = http.createServer(function(request, response) {
   } else {
     response.statusCode = 404;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`你输入的路径不存在对应的内容`);
+    response.write(`你访问的页面不存在`);
     response.end();
   }
 
